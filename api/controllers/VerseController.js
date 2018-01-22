cd1d55ba76ebd825be9273258bb126cb4727fea6
rogerrw <roger.wang.r@gmail.com>
2018-01-22 17:06:13 -0500

    add bible verse model

---
+++ api/controllers/VerseController.js
@@ -0,0 +1,11 @@
+/**
+ * Bible_verseController
+ *
+ * @description :: Server-side logic for managing bible_verses
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
