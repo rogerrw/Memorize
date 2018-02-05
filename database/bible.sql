ec8f48e083345211018c6d73edc702bb62a65f6a
Roger Wang <roger.wang.r@gmail.com>
2018-02-05 16:10:35 -0500

    remove create tables in seed sql script

--- database/bible.sql
+++ database/bible.sql
@@ -1,35 +1,3 @@
-CREATE SCHEMA bible;
-
-CREATE TABLE bible.genres (
-  id INTEGER,
-  genre VARCHAR,
-  PRIMARY KEY (id)
-);
-CREATE TABLE bible.key (
-  id INTEGER,
-  book_name VARCHAR,
-  testament CHAR(2),
-  genre INTEGER,
-  PRIMARY KEY(id)
-);
-
-CREATE TABLE bible.abbreviations (
-  id INTEGER,
-  abbreviation VARCHAR,
-  book_id INTEGER,
-  preferred SMALLINT,
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE bible.kjv (
-  verse_id SERIAL,
-  book_id INTEGER,
-  chapter INTEGER,
-  verse_number INTEGER,
-  verse_text TEXT,
-  PRIMARY KEY(verse_id)
-);
-
 COPY bible.genres FROM '/full/path/to/data/bible_genres.csv' WITH (FORMAT csv);
 COPY bible.key FROM '/full/path/to/data/bible_key.csv' WITH (FORMAT csv);
 COPY bible.abbreviations FROM '/full/path/to/data/bible_abbreviations.csv' WITH (FORMAT csv);
