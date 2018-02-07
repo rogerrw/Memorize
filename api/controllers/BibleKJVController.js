096191c11447bd7661f7c2b182253c2384992236
Roger Wang <roger.wang.r@gmail.com>
2018-02-07 10:59:46 -0500

    view now gets controller data

--- api/controllers/BibleKJVController.js
+++ api/controllers/BibleKJVController.js
@@ -6,8 +6,19 @@
 
 module.exports = {
 	lookUp: function (req, res) {
-		debugger;
-		BibleKJV.findById();
-		return res.view('verse', {});
+		BibleKJV.findAll({
+			attributes: ['book', 'chapter', 'verse', 'verseText'],
+			where: {
+				book: 1,
+				chapter: 1,
+				verse: 1
+			}
+		}).then( results => {
+			return res.view('lookup', {
+				firstResult: results[0].dataValues.verseText
+			});
+		}).catch( function(err) {
+			return res.serverError(err);
+		});
 	},
 };
--- api/models/BibleKJV.js
+++ api/models/BibleKJV.js
@@ -11,7 +11,7 @@ module.exports = {
       primaryKey: true
     },
     book: {
-      type: Sequelize.STRING,
+      type: Sequelize.INTEGER,
       allowNull: false
     },
     chapter: {
